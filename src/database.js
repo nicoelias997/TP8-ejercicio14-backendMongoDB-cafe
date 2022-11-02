2a8adc33a58abae0ef9492e9ad4ecfd7df562963
nicoelias997 <nicoelias997@gmail.com>
2022-11-02 11:33:13 +0100

    Cambie la url para enlazar mongo compass al backend

--- src/database.js
+++ src/database.js
@@ -1,7 +1,8 @@
 import mongoose from "mongoose";
 
 //aqui nos conectaremos, y le pedimos a la libreria que nos conectemos... cafe-.. es el nombre de nuestra base de datos
-const url = "mongodb://localhost:27017/cafe-benito-santos"
+// const url = "mongodb://localhost:27017/cafe-benito-santos"/local
+const url = "mongodb+srv://nicoelias997:nicoelias997@cluster0.qo1exxt.mongodb.net/cafebenitosantos"//produccion
 
 mongoose.connect(url)
 
